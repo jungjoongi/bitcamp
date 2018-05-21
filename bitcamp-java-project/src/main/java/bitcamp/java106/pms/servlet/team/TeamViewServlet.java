07ee8aba97f243d47bf0c56121605c838a2efeba
zx3620 <zx3620@gmail.com>
2018-05-21 11:13:16 +0900

    2015.05.21

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamViewServlet.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/servlet/team/TeamViewServlet.java
@@ -23,12 +23,10 @@ import bitcamp.java106.pms.servlet.InitServlet;
 public class TeamViewServlet extends HttpServlet {
 
     TeamDao teamDao;
-    TeamMemberDao teamMemberDao;
     
     @Override
     public void init() throws ServletException {
         teamDao = InitServlet.getApplicationContext().getBean(TeamDao.class);
-        teamMemberDao = InitServlet.getApplicationContext().getBean(TeamMemberDao.class);
     }
     
     @Override
@@ -91,29 +89,11 @@ public class TeamViewServlet extends HttpServlet {
             out.println("</p>");
             out.println("</form>");
             
-            List<Member> members = teamMemberDao.selectListWithEmail(name);
+            // 팀 회원의 목록을 출력하는 것은 TeamMemberListServlet에게 맡긴다.
+            RequestDispatcher 요청배달자 = request.getRequestDispatcher("/team/member/list");
+            요청배달자.include(request, response);
             
-            out.println("<h2>회원 목록</h2>");
-            out.println("<form action='member/add' method='post'>");
-            out.println("<input type='text' name='memberId' placeholder='회원아이디'>");
-            out.printf("<input type='hidden' name='teamName' value='%s'>", name);
-            out.println("<button>추가</button>");
-            out.println("</form>");
-            out.println("<table border='1'>");
-            out.println("<tr><th>아이디</th><th>이메일</th></tr> </th></tr>");
-            for (Member member : members) {
-                out.printf("<tr>"
-                        + "<td>%s</td>"
-                        + "<td>%s</td>"
-                        + "<td><a href='member/delete?teamName=%s&memberId=%s'>삭제</a></td>"
-                        + "</tr>\n",
-                        member.getId(),
-                        member.getEmail(),
-                        name,
-                        member.getId());
-            }
-            out.println("</table>");
-               
+            // TeamMemberServlet이 작업을 수행한 후 이 서블릿으로 되돌아온다.
         } catch (Exception e) {
             RequestDispatcher 요청배달자 = request.getRequestDispatcher("/error");
             request.setAttribute("error", e);
