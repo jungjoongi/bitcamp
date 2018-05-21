b66f4c9d3a3275d3a6f31492de1c81cf8f06891f
zx3620 <zx3620@gmail.com>
2018-05-21 15:25:11 +0900

    2018.05.21

--- bitcamp-web01/src/main/java/step09/ex1/Filter3.java
+++ bitcamp-web01/src/main/java/step09/ex1/Filter3.java
@@ -1,3 +1,4 @@
+// 웹 애플리케이션 컴포넌트 : FilterConfig의 활용
 package step09.ex1;
 
 import java.io.IOException;
@@ -8,11 +9,10 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 
-@WebFilter("/step09/ex1/exam03")
+// 애노테이션 대신에 web.xml에 배치 정보를 설정한다.
+//@WebFilter("/step09/ex1/exam03") 
 public class Filter3 implements Filter {
-    
     FilterConfig config;
     
     @Override
@@ -25,14 +25,22 @@ public class Filter3 implements Filter {
     }
     
     @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(
+            ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        
+        // FilterConfig 도구를 사용하여 web.xml에 설정된 파라미터 값을 가져올 수 있다.
         String v1 = config.getInitParameter("v1");
         String v2 = config.getInitParameter("v2");
-        
         System.out.printf("v1=%s, v2=%s\n", v1, v2);
         
         chain.doFilter(request, response);
     }
     
 }
+
+
+
+
+
+
