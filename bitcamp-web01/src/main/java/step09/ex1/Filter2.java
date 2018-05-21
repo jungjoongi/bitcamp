e8befe2195fe5d6547cc3cf3b986ec8de6930adc
zx3620 <zx3620@gmail.com>
2018-05-21 12:00:06 +0900

    2018.05.21

---
+++ bitcamp-web01/src/main/java/step09/ex1/Filter2.java
@@ -0,0 +1,38 @@
+package step09.ex1;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter("/step09/ex1/exam02")
+public class Filter2 implements Filter {
+    
+    FilterConfig config;
+    
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.config = filterConfig;
+        System.out.println("Filter2.init()"); 
+    }
+    
+    @Override
+    public void destroy() {
+        System.out.println("Filter2.destroy()");
+    }
+    
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Filter2.doFilter() : before");
+        
+        chain.doFilter(request, response);
+        System.out.println("Filter2.doFilter() : after");
+    }
+    
+}
