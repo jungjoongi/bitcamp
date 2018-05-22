02f8ce86944970cf286785caec9fee6c9cd360d2
zx362 <zx362@DESKTOP-3VLL8LN>
2018-05-22 13:53:58 +0900

    2018.05.22

--- bitcamp-web01/src/main/java/step09/ex2/Exam01.java
+++ bitcamp-web01/src/main/java/step09/ex2/Exam01.java
@@ -3,7 +3,6 @@ package step09.ex2;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
