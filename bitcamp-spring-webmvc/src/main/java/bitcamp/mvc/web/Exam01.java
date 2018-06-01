19acb0f5016feadd05f4f6dc35a7f573c04a8f50
zx3620 <zx3620@gmail.com>
2018-06-01 14:25:40 +0900

    2018.06.01

--- bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam01.java
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/Exam01.java
@@ -2,12 +2,14 @@ package bitcamp.mvc.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller("web.HelloController")
-public class HelloController {
+public class Exam01 {
     
-    @RequestMapping("/hello")
-    public String hello() {
-        return "/hello.jsp";
+    @RequestMapping("/exam01/m1")
+    @ResponseBody
+    public String m1() {
+        return "hello!";
     }
 }
---
+++ bitcamp-spring-webmvc/src/main/java/bitcamp/mvc/web/MyWebApplicationInitializer.java
@@ -0,0 +1,17 @@
+
+package bitcamp.mvc.web;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import org.springframework.web.WebApplicationInitializer;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // 웹어플리케이션을 시작할 때 이 메서드를 호출한다. 누가?
+        System.out.println("===> MyWebApplicationInitializer.onStart()");
+    }
+
+}
