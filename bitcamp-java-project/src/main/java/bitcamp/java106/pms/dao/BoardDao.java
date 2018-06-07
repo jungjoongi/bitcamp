d13494e8aac0453801aaa290af97d5179b9bca89
zx3620 <zx3620@gmail.com>
2018-06-07 17:10:27 +0900

    @MatrixVariable

--- bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
+++ bitcamp-java-project/src/main/java/bitcamp/java106/pms/dao/BoardDao.java
@@ -1,13 +1,14 @@
 package bitcamp.java106.pms.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import bitcamp.java106.pms.domain.Board;
 
 public interface BoardDao {
     
     public int delete(int no);
-    public List<Board> selectList();
+    public List<Board> selectList(Map<String,Object> params);
     public int insert(Board board);
     public int update(Board board);
     public Board selectOne(int no);
