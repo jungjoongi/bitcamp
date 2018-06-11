15b9e0fcd722f7abb54c6642a33a4b99624e7cfb
zx3620 <zx3620@gmail.com>
2018-06-11 16:30:29 +0900

    2018.06.11

---
+++ bitcamp-java-basic/src/step25/ex9/Exam01_list.java
@@ -0,0 +1,32 @@
+// mybatis + spring IoC - 트랜잭션 적용 전
+package step25.ex9;
+
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Exam01_list {
+
+    public static void main(String[] args) throws Exception {
+        ClassPathXmlApplicationContext iocContainer = 
+                new ClassPathXmlApplicationContext(
+                        "step25/ex8/application-context.xml");
+        
+        BoardDao boardDao = iocContainer.getBean(BoardDao.class);
+        
+        System.out.println("-------------------------------------");
+        
+        List<Board> list = boardDao.selectList();
+        for (Board b : list) {
+            System.out.printf("%d, %s, %s\n",
+                    b.getNo(), b.getTitle(), b.getRegisteredDate());
+        }
+    }
+}
+
+
+
+
+
+
+
