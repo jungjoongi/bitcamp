297279ea0f55ba10c7e64580df0cd69666e82bdc
zx3620 <zx3620@gmail.com>
2018-06-15 14:21:20 +0900

    2018.06.15

--- bitcamp-java-basic/src/step25/ex10/BoardService.java
+++ bitcamp-java-basic/src/step25/ex10/BoardService.java
@@ -2,11 +2,13 @@ package step25.ex10;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BoardService {
     @Autowired BoardDao boardDao;
     
+    @Transactional
     public void test1(Board b1, Board b2, Board b3) {
         boardDao.insert(b1);
         System.out.printf("%d번 게시물 입력!\n", b1.getNo());
--- bitcamp-java-basic/src/step25/ex10/Exam01_insert.java
+++ bitcamp-java-basic/src/step25/ex10/Exam01_insert.java
@@ -1,4 +1,3 @@
-// mybatis + spring IoC - 트랜잭션 적용 전
 package step25.ex10;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,27 +7,27 @@ public class Exam01_insert {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext iocContainer = 
                 new ClassPathXmlApplicationContext(
-                        "step25/ex9/application-context.xml");
+                        "step25/ex10/application-context.xml");
         
         
         BoardService boardService = iocContainer.getBean(BoardService.class);
         
         Board b1 = new Board();
-        b1.setNo(158);
+        b1.setNo(171);
         b1.setTitle("1111");
         b1.setContent("xxxx");
         
         Board b2 = new Board();
-        b2.setNo(159);
+        b2.setNo(172);
         b2.setTitle("1111");
         b2.setContent("xxxx");
 
         Board b3 = new Board();
-        b3.setNo(153);
+        b3.setNo(171);
         b3.setTitle("1111");
         b3.setContent("xxxx");
         
-        boardService.test1(b1, b2, b3);
+        boardService.test2(b1, b2, b3);
         System.out.println("입력성공");
     }
 }
