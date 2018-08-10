6d5e313091bda5cfbeb757e09c9a771215ebbe09
zx3620 <zx3620@gmail.com>
2018-08-10 16:25:34 +0900

    chatbot

--- bitcamp-facebook-chatbot/app.js
+++ bitcamp-facebook-chatbot/app.js
@@ -114,13 +114,13 @@ function handleMessage(sender_psid, received_message) {
   let response;
   
   // Checks if the message contains text
-  if (received_message.text == hello) {    
+  if (received_message.text == 'hello') {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
       "text": `나도반가워요 ㅋㅋ`
     }
-  } else if (received_message.text == menu) {
+  } else if (received_message.text == 'menu') {
     response = {
     	      "attachment": {
     	        "type": "template",
