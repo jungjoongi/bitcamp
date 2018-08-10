8c6ede6d07d0ae2f1f732dfbdfe5f67718ef0c41
zx3620 <zx3620@gmail.com>
2018-08-10 16:04:32 +0900

    chatbot

--- bitcamp-facebook-chatbot/app.js
+++ bitcamp-facebook-chatbot/app.js
@@ -86,7 +86,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = process.env.VERYFY_TOKEN;
+  const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
